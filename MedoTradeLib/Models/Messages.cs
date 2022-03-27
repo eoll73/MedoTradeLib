d92e141bd73216b77805eab73d499e6042b9f0e2
Victor <vittorio073@mail.ru>
2022-03-27 20:49:16 +0300

    My test application

---
+++ MedoTradeLib/Models/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedoTradeLib.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Client { get; set; }
+        public string Book { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
---
+++ MedoTradeLib/Repositories/MessagesRepository.cs
@@ -0,0 +1,52 @@
+﻿using MedoTradeLib.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedoTradeLib.Repositories
+{
+    public class MessagesRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly LibDbContext context;
+
+        public MessagesRepository(IConfiguration configuration, LibDbContext context)
+        {
+            _configuration = configuration;
+            this.context = context;
+        }
+
+        public List<Messages> GetAllMessages()
+        {
+            var messages = context.Messages.ToList();
+            return messages;
+        }
+
+        public void Add(Messages messages)
+        {
+            context.Messages.Add(messages);
+            context.SaveChanges();
+        }
+
+        public void Update(Messages messages)
+        {
+            context.Messages.Update(messages);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var message = context.Messages.FirstOrDefault(x => x.Id == id);
+
+            context.Messages.Remove(message);
+            context.SaveChanges();
+        }
+
+        public Messages Get(int id)
+        {
+            return (context.Messages.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
